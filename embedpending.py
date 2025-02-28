473fbd9744d38463193eea0190d34fd9ddabba5e
howff <howff>
2025-02-28 13:51:31 +0000

    fix embedpending.py, esp. ACDDBUPOFF value

--- embedpending.py
+++ embedpending.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
+# Read the database and display all files which have Embed Pending flag
+# (ACDDBUPOFF non-zero)
+
 import sys
 import glob
 import os
 from dbfread2 import DBF, FieldParser
 
-rootdir='/mnt/cifs/documents/Backup/ACDSee/170Ult/Default'
+#catalogdir='/mnt/cifs/documents/Backup/ACDSee/170Ult/Default'
+catalogdir="c:\\Users\\arb\\AppData\\Local\\ACD Systems\\Catalogs\\170Ult\\Default"
 
 folder = dict()
 folder_parent = dict()
@@ -33,12 +37,13 @@ class TestFieldParser(FieldParser):
         #print(field.name, data)
         return data
 
-read_folders(rootdir + '/Folder.dbf')
+read_folders(catalogdir + '/Folder.dbf')
 
-for dbffile in glob.glob(rootdir + '/Asset.dbf'):
+for dbffile in glob.glob(catalogdir + '/Asset.dbf'):
     print('Extract %s' % (dbffile))
     table = DBF(dbffile, parser_class=TestFieldParser, encoding='cp437', ignore_missing_memo=True)
     for record in table:
-        if record['ACDDBUPOFF']:
+        if record['ACDDBUPOFF'] > 0:
             #print('%s in %s rating %s' % (record['NAME'], record['FOLDER_ID'], record['RATING']))
-            print('%s %s rating %s' % (folder_path(record['FOLDER_ID']), record['NAME'], record['RATING']))
+            print('%s%s flag %s rating %s' % (folder_path(record['FOLDER_ID']), record['NAME'],
+                record['ACDDBUPOFF'], record['RATING']))
