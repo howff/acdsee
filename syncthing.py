63a31d5de7fa05126d449a8dc9e701914caa0e4d
howff <howff@github>
2025-04-24 11:28:15 +0000

    syncthing.py - refactored main() so that pytest can be used, written test functions, improved the configuration variable names, added debug and verbose options (the latter not used right now), the directory prefix is only checked for root directories not subdirectories, files can be added to the list to be copied even if their directory has already been copied if they have been modified since the last time their directory was copied.

--- syncthing.py
+++ syncthing.py
@@ -14,9 +14,11 @@
 # TO DO!
 #  1. the check step should add up file sizes and
 #     ask before copying especially if multiple GB.
-# Write the CSV properly not just append to file
-# Keep CSV sorted
-# Add a 'date copied' field to the CSV
+#  2. check the file date and if it's newer than the last time
+#     its directory was copied then copy it (otherwise it would
+#     be ignored because it's directory was previously copied).
+# Write the CSV properly not just append to file.
+# Keep CSV sorted.
 # Use a database like sqlite not a CSV?
 
 import argparse
@@ -26,17 +28,29 @@ import os
 import re
 import shutil
 import sys
+import tempfile
 import time
 
+debug=False
+verbose=False
 #srcdir="/mnt/cifs/pictures/ixus"
 srcdir="/mnt/cifs/backup_ro/dell9500_backup/Pictures"
 destdir="/mnt/cifs/pictures/Pixel_Camera_Sync"
+max_days=250           # don't sync anything older than max_days
+min_size=1024          # probably not an image
+max_size=100*1024*1024 # 100MB is too large for an image
+dir_prefix='202'       # Use 202 for 2020 onwards, or None to include all dirs and subdirs
 database="synced.csv"
-days=100
+
 
 # ---------------------------------------------------------------------
 def relative_dir_to_src(filename):
+    """ Return the directory for filename but without the srcdir prefix """
     return os.path.dirname(filename).replace(srcdir+'/', '')
+
+def test_relative_dir_to_src():
+    assert(relative_dir_to_src(os.path.join(srcdir, "me", "you", "file.jpg")) == 'me/you')
+
 
 # ---------------------------------------------------------------------
 def image_rating(filename):
@@ -56,20 +70,48 @@ def image_rating(filename):
                 rating = None
     return rating
 
+def test_image_rating():
+    assert(image_rating('/dev/null') == None)
+    with tempfile.NamedTemporaryFile() as fd:
+        fd.write('stuff <x:xmpmeta> stuff <acdsee:rating>3</acdsee:rating> stuff </x:xmpmeta> stuff'.encode())
+        fd.flush()
+        assert(image_rating(fd.name) == 3)
+
+
 # ---------------------------------------------------------------------
-def read_database():
+def read_database(database):
+    """ Read synced.csv to get updateddate,path into a dictionary
+    indexed by the path and return the dict. """
     db = {}
     if not os.path.isfile(database):
         return db
-    with open(database) as fd:
-        csvr = csv.DictReader(fd)
+    with open(database, 'r', newline='') as fd:
+        csvr = csv.DictReader(fd, delimiter='\t')
         for row in csvr:
-            db[row['path']] = 1
+            # N.B. if path appears twice then the subsequent ones
+            # override the earlier ones, which is handy because they
+            # probably contain a more recent updateddate (although we
+            # don't actually check the date, but we could).
+            db[row['path']] = time.strptime(row['updateddate'], '%Y-%m-%d')
     return db
+
+def test_read_database():
+    with tempfile.NamedTemporaryFile() as fd:
+        fd.write('updateddate\tpath\n2020-02-02\tdir1/dir2\n'.encode())
+        fd.flush()
+        assert(read_database(fd.name) == {'dir1/dir2': time.strptime('2020-02-02','%Y-%m-%d')})
 
 
 # ---------------------------------------------------------------------
 def find_files_to_copy(db):
+    """ Recursively find files under 'srcdir' and return a tuple
+    (files_to_copy[path: str], bytes_to_copy[int]). Ignores
+    the [Originals] directory, files too small or too large,
+    files older than 'max_days', files not .jpg, files without an
+    ACDSee rating, and files in a directory which has already
+    been synced (passed in 'db') unless file has been modified
+    more recently than the previous sync date.
+    """
     time_now = time.time()
 
     files_to_copy=[]
@@ -77,6 +119,8 @@ def find_files_to_copy(db):
 
     prevprinted=None
     for root, dirs, files in os.walk(srcdir):
+        if dir_prefix and root == dir_prefix:
+            dirs[:] = [d for d in dirs if d.startswith(dir_prefix)]
         for name in files:
             # Ignore if an ACDSee backup directory
             if '[Originals]' in root:
@@ -84,21 +128,27 @@ def find_files_to_copy(db):
             # Ignore if not a JPEG file
             if (not name.endswith('.JPG')) and (not name.endswith('.jpg')):
                 continue
-            # Ignore if this directory has already been copied
+            # Ignore if too old
+            if time_now - os.path.getmtime(fullpath) > 86400 * max_days:
+                if debug: print('IGNORE_TOO_OLD %s' % fullpath)
+                continue
+            # Ignore if this directory has already been copied.
+            # Unless the file has been modified since the directory was last copied.
             fullpath = os.path.join(root, name)
             dire = relative_dir_to_src(fullpath)
             if dire in db:
-                continue
-            # Ignore if too old
-            if time_now - os.path.getmtime(fullpath) > 86400 * days:
-                continue
+                if os.path.getmtime(fullpath) < db[dire]:
+                    if debug: print('IGNORE_ALREADY_SYNCED %s on %s via %s' % (fullpath, db[dire], dire))
+                    continue
             # Ignore if too small or too large
             filesize = os.path.getsize(fullpath)
-            if filesize < 1024 or filesize > 100*1024*1024:
+            if filesize < min_size or filesize > max_size:
+                if debug: print('IGNORE_TOO_SMALL %s' % fullpath)
                 continue
             # Ignore if not rated 1..5 in ACDSee
             rating = image_rating(fullpath)
             if not rating:
+                if debug: print('IGNORE_NOT_RATED %s' % fullpath)
                 continue
             # Add to list
             files_to_copy += [fullpath]
@@ -109,70 +159,84 @@ def find_files_to_copy(db):
                 prevprinted = dire
     return files_to_copy, bytes_to_copy
 
+
 # ---------------------------------------------------------------------
-parser = argparse.ArgumentParser(description='syncthing wrapper')
-parser.add_argument('-v', '--verbose', action="store_true", help='verbose')
-parser.add_argument('--copy', action="store_true", help='actually perform the copy (otherwise only lists what will be copied)')
-parser.add_argument('--log', action="store", help='store progress in the given log file')
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description='syncthing wrapper')
+    parser.add_argument('-d', '--debug', action="store_true", help='debug')
+    parser.add_argument('-v', '--verbose', action="store_true", help='verbose')
+    parser.add_argument('--copy', action="store_true", help='actually perform the copy (otherwise only list what will be copied)')
+    parser.add_argument('--log', action="store", help='store progress in the given log file')
+    args = parser.parse_args()
 
-if args.log:
-    logfd = open(args.log, 'a')
-else:
-    logfd = open('/dev/null', 'w')
+    if args.debug: debug=True
+    if args.verbose: verbose=True
 
-timenow = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-print('%s Starting' % timenow)
+    if args.log:
+        logfd = open(args.log, 'a')
+    else:
+        logfd = open('/dev/null', 'w')
 
-print('READING DATABASE')
-print('LOAD %s' % database, file=logfd)
-db = read_database()
+    timenow = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+    print('%s Starting' % timenow)
 
-print('FIND FILES TO COPY')
-files_to_copy, bytes_to_copy = find_files_to_copy(db)
+    print('READING DATABASE')
+    print('LOAD %s' % database, file=logfd)
+    db = read_database(database)
 
-print()
-dirs_to_copy=set()
-for file in files_to_copy:
-    dire = relative_dir_to_src(file)
-    dirs_to_copy.add(dire)
+    print('FIND FILES TO COPY')
+    files_to_copy, bytes_to_copy = find_files_to_copy(db)
+    files_to_copy = sorted(files_to_copy)
 
-for dire in dirs_to_copy:
-    print('COPYDIR %s' % dire)
-    print('COPYDIR %s' % dire, file=logfd)
-print("Total size to copy = %f MB" % (bytes_to_copy / 1024 / 1024))
-print("Total size to copy = %f MB" % (bytes_to_copy / 1024 / 1024), file=logfd)
+    print()
+    dirs_to_copy=set()
+    for file in files_to_copy:
+        dire = relative_dir_to_src(file)
+        dirs_to_copy.add(dire)
+    dirs_to_copy = sorted(dirs_to_copy) # now a list
 
-if not args.copy:
+
+    for dire in dirs_to_copy:
+        print('COPYDIR %s' % dire)
+        print('COPYDIR %s' % dire, file=logfd)
+    print("Total size to copy = %f MB" % (bytes_to_copy / 1024 / 1024))
+    print("Total size to copy = %f MB" % (bytes_to_copy / 1024 / 1024), file=logfd)
+
+    if not args.copy:
+        timenow = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+        print('%s Finished' % timenow)
+        print('Run with --copy next time')
+        sys.exit(0)
+
+    print('CREATE DIRECTORIES')
+    for dire in dirs_to_copy:
+        #print('MKDIR %s' % os.path.join(destdir, dire))
+        print('MKDIR %s' % os.path.join(destdir, dire), file=logfd)
+        os.makedirs(os.path.join(destdir, dire), exist_ok=True)
+
+    print('COPY')
+    for file in files_to_copy:
+        dire = relative_dir_to_src(file)
+        dire = os.path.join(destdir, dire)
+        print('COPY %s -> %s' % (file,dire))
+        print('COPY %s -> %s' % (file,dire), file=logfd)
+        shutil.copy2(file, dire)
+
+    print('UPDATE DATABASE')
+    print('UPDATE %s' % database, file=logfd)
+    write_header = False if os.path.isfile(database) else True
+    now = datetime.today().strftime('%Y-%m-%d')
+    with open(database, 'a') as fd:
+        if write_header:
+            print('updateddate\tdirectory', file=fd)
+        csvw = csv.writer(fd, delimiter='\t')
+        for dire in dirs_to_copy:
+            csvw.writerow( [now, dire] )
+
     timenow = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
     print('%s Finished' % timenow)
-    sys.exit(0)
 
-print('CREATE DIRECTORIES')
-for dire in dirs_to_copy:
-    #print(destdir)
-    #print(dire)
-    #print('MKDIR %s' % os.path.join(destdir, dire))
-    print('MKDIR %s' % os.path.join(destdir, dire), file=logfd)
-    os.makedirs(os.path.join(destdir, dire), exist_ok=True)
 
-print('COPY')
-for file in files_to_copy:
-    dire = relative_dir_to_src(file)
-    dire = os.path.join(destdir, dire)
-    print('COPY %s -> %s' % (file,dire))
-    print('COPY %s -> %s' % (file,dire), logfd)
-    shutil.copy2(file, dire)
-
-print('UPDATE DATABASE')
-print('UPDATE %s' % database, file=logfd)
-with open(database, 'a') as fd:
-    for dire in dirs_to_copy:
-        # Not properly writing as CSV, need to quote paths containing comma,
-        if ',' in dire:
-            print('"%s"' % dire, file=fd)
-        else:
-            print('%s' % dire, file=fd)
-
-timenow = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-print('%s Finished' % timenow)
+# ---------------------------------------------------------------------
+if __name__ == '__main__':
+    main()
